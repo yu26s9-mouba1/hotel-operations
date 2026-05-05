67d92894090c162accf9b6becc0c6a3ed89616e7
Moubarak Congacou <cmouba1999@gmail.com>
2026-05-05 10:53:17 -0400

    Added comments

--- hotel-operations/src/main/java/com/pluralsight/Main.java
+++ hotel-operations/src/main/java/com/pluralsight/Main.java
@@ -4,13 +4,18 @@ public class Main {
     public static void main(String[] args) {
 
 
-
+        /*Testing the room
+        *
+         */
         Room room1 = new Room(2, 150.00, false, false);
         System.out.println("Room available: " + room1.isAvailable());
 
+        // Testing the reservation
         Reservation reservation1 = new Reservation("king", 3, true);
         System.out.println("Reservation total: $" + reservation1.getReservationTotal());
 
+
+        // Testing the employee object
         Employee employee1 = new Employee(101, "Mubarak", "IT", 20.0, 45);
         System.out.println("Regular hours: " + employee1.getRegularHours());
         System.out.println("Overtime hours: " + employee1.getOvertimeHours());
